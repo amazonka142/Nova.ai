7a21d8bd7360a33e6deaa5936ae906cdce7a790d
novaProjectV <dayerteq42@gmail.com>
2026-01-24 20:49:56 +0300

    Initial Commit

---
+++ Nova.ai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nova.ai
+//
+//  Created by Vlad on 1/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nova.ai/Nova_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nova_aiApp.swift
+//  Nova.ai
+//
+//  Created by Vlad on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Nova_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
